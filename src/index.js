0b189ecd0d2f65d8cad52f6e45227a7376df09d7
lindalopezc <lindalopezramos61@gmail.com>
2022-02-17 20:14:51 -0500

    Card-validation-Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
 import validator from './validator.js';
 
-console.log(validator);
+let message = document.getElementById('message');
+const cardButton= document.getElementById('cardButton');
+    cardButton.addEventListener('click', playButton);
+
+function playButton(){
+    let cardNumber = document.getElementById('cardNumber').value;
+
+    if (cardNumber){ 
+        
+        const finalAnswer = validator.isValid(cardNumber);
+        const hiddenCard = validator.maskify(cardNumber);
+
+        if(finalAnswer){
+            message.innerHTML= '';
+            message.innerHTML= 'La tarjeta '+ hiddenCard + ' es válida. Gracias';
+        }
+        else{
+            message.innerHTML= '';
+            message.innerHTML= 'La tarjeta ' + hiddenCard + ' no es válida.' + '<br>' + 'Por favor vuelva a intentarlo';
+        }
+        console.log(finalAnswer);
+    }
+    else{
+        message.innerHTML= '';
+        message.innerHTML= 'Campo vacío. Por favor ingrese su número de tarjeta';    
+    }
+}
+
+let buyButton = document.getElementById('buyButton');
+buyButton.addEventListener('click',showFormulary);
+
+function showFormulary(){
+    document.getElementById('presentation').style.display='none';
+    document.getElementById('card').style.display ='block';
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,52 @@
+/* eslint-disable no-console */
 const validator = {
-  // ...
-};
+  isValid:  function(card){
+    let arrayOfCard = Array.from(card);
+    let newArray = []; 
+    let totalSum = 0;
+    for (let i = 0; i < arrayOfCard.length; i++){
+        newArray.push(parseInt(arrayOfCard[i]));
+    }
+    newArray.reverse();
+    
+    console.log('Estos son los dígitos invertidos: ');
+    console.log(newArray);       
+    
+    for(let i = 0; i< newArray.length ; i++){
+        if (i%2 == 1){
+        newArray[i] = newArray[i]*2;
+        if (newArray[i] >= 10){
+            newArray[i] = newArray[i]-9;
+            }    
+        }
+       totalSum = totalSum + newArray[i];
+    }
+    console.log('Al duplicar las cifras de lugar par y sumar los digitos si son mayores a 10, se obtiene :');
+    console.log(newArray);
+    console.log('La suma de los dígitos de este arreglo es:');
+    console.log(totalSum);
+        
+    if (totalSum % 10 == 0){
+        return true;
+    }  
+    else{
+        return false;
+    }
+  },
+  maskify:function(card){
 
+    const arrayOfCard = Array.from(card);
+    let hiddenCard = '';
+    if(arrayOfCard.length <= 4){
+      hiddenCard = arrayOfCard.join('');
+    }
+    else{
+      for(let i = 0; i < arrayOfCard.length-4; i++){
+      arrayOfCard[i] = '#';
+      }
+      hiddenCard = arrayOfCard.join('');
+    }
+    return hiddenCard;
+  }
+}
 export default validator;
